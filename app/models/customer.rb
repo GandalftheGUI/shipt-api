45df8b2c10749e51b086a48d012abada937cadd9
Ian Remillard <Doom-Mac@Ians-iMac.local>
2018-02-13 17:27:33 -0800

    query working

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders
 
-
   def self.customer_order_count_by_category
-    
+    Order.all.joins({:order_items => {:product => :categories}}).joins(:customer).group([:customer_id, :first_name, :category_id, "categories.name"]).count
   end
 end
